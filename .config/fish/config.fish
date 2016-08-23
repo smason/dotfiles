ed4bcb6e2d381f0297d060ccdf75be9cc836af53
Sam Mason <sam.mason@consultantconnect.org.uk>
2016-08-24 00:09:01 +0100

    split up fish config a bit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,19 @@
 set -gx fish_greeting ''
 
-set -gx PATH ~/bin ~/.npm-packages/bin $PATH
+# various path changes
+if [ -d "$HOME/android-sdk" ]
+  set -gx ANDROID_HOME "$HOME/android-sdk"
+  set -gx PATH "$ANDROID_HOME/tools" "$ANDROID_HOME/platform-tools" $PATH
+end
+
+# node
+if [ -d "$HOME/.npm-packages" ]
+   set -gx PATH "$HOME/.npm-packages/bin" $PATH
+end
+
+if [ -d "$HOME/bin" ]
+   set -gx PATH "$HOME/bin" $PATH
+end
 
 if command --search src-hilite-lesspipe.sh > /dev/null
   set -gx LESSOPEN "| src-hilite-lesspipe.sh %s"
