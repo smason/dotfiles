f73827c52e9217a87e65f21a67fa626b50981d70
Sam Mason <sam@samason.uk>
2016-08-25 15:26:43 +0100

    added iterm2 shell integration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,14 +11,21 @@ if [ -d "$HOME/.npm-packages" ]
    set -gx PATH "$HOME/.npm-packages/bin" $PATH
 end
 
+# final fiddling with path
 if [ -d "$HOME/bin" ]
    set -gx PATH "$HOME/bin" $PATH
 end
 
+# Less config
 if command --search src-hilite-lesspipe.sh > /dev/null
   set -gx LESSOPEN "| src-hilite-lesspipe.sh %s"
   set -gx LESS "-R"
 end
 
+# Python
 set -gx PIP_REQUIRE_VIRTUALENV true
 eval (/usr/local/bin/python3 -m virtualfish)
+
+# iTerm2 shell integration
+# https://iterm2.com/documentation-shell-integration.html
+source "$HOME/.config/fish/iterm2_startup.fish"
