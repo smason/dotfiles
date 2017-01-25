ebc9b0dec8a088631ed9c9feea3fbb0da8fcec6f
Sam Mason <sam@samason.uk>
2017-01-25 21:32:24 +0000

    OSX config was breaking Linux, fix!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,10 +13,9 @@ set -x __fish_git_prompt_showdirtystate 1
 #  https://wiki.archlinux.org/index.php/GnuPG
 #  https://github.com/gpg/gnupg/tree/master/doc/examples/systemd-user
 if command -s gpg-connect-agent >/dev/null
-  # compatibility with OSX
-  set -l SSH_AUTH_SOCK "$HOME/.gnupg/S.gpg-agent.ssh"
-  if [ -S "$SSH_AUTH_SOCK" ]
-    set -x SSH_AUTH_SOCK
+  # compatibility with OSX, replace OSX Keychain SSH helper with GPG
+  if [ (uname -s) = Darwin ]
+    set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
   end
   # get curses pinentry's in the right place
   set -x GPG_TTY (tty)
