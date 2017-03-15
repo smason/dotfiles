1797ee29c966014c19c2885aad02362cf0c20bb3
Sam Mason <sam@samason.uk>
2017-03-15 12:46:26 +0000

    use Bluez media control dbus messages to control headset volume

--- bin/pulse-volume.py
+++ bin/pulse-volume.py
@@ -1,9 +1,21 @@
 import sys
+import subprocess
+
 from pulsectl import Pulse
 
 [_,diff_str] = sys.argv
 diff = float(diff_str)
 
-with Pulse('volume-increaser') as pulse:
+with Pulse('volume-changer') as pulse:
     for sink in pulse.sink_list():
-        pulse.volume_change_all_chans(sink, diff)
+        bluez_path = sink.proplist['bluez.path']
+        if bluez_path:
+            args = [
+                'dbus-send', '--system', '--print-reply',
+                '--dest=org.bluez', bluez_path,
+                'org.bluez.MediaControl1.Volume{}'.format(
+                    'Down' if diff < 0 else 'Up'),
+            ]
+            subprocess.run(args, check=True)
+        else:
+            pulse.volume_change_all_chans(sink, diff)
