0048744dc661df05c3a7ff4314eccbe37ec2a110
Sam Mason <sam@samason.uk>
2017-03-15 15:48:50 +0000

    modify non-bluetooth audio volume on a log scale

--- bin/pulse-volume.py
+++ bin/pulse-volume.py
@@ -1,21 +1,35 @@
 import sys
 import subprocess
 
-from pulsectl import Pulse
+from pulsectl import Pulse, PulseVolumeInfo
 
 [_,diff_str] = sys.argv
 diff = float(diff_str)
 
+def formatVolume(values):
+    return "[{}]".format(', '.join(
+        '{:.2g}'.format(v) for v in values))
+
 with Pulse('volume-changer') as pulse:
     for sink in pulse.sink_list():
-        bluez_path = sink.proplist['bluez.path']
-        if bluez_path:
+        if 'bluez.path' in sink.proplist:
             args = [
                 'dbus-send', '--system', '--print-reply',
-                '--dest=org.bluez', bluez_path,
+                '--dest=org.bluez', bluez_path['bluez.path'],
                 'org.bluez.MediaControl1.Volume{}'.format(
                     'Down' if diff < 0 else 'Up'),
             ]
             subprocess.run(args, check=True)
         else:
-            pulse.volume_change_all_chans(sink, diff)
+            old = sink.volume
+
+            # perceived-volume is a logarithmic quantity
+            mul = 2 ** diff
+            new = [v * mul for v in old.values]
+
+            # set the volume
+            pulse.volume_set(sink, PulseVolumeInfo(new))
+
+            # display something nice
+            print("volume changed: {old} => {new}".format(
+                old=formatVolume(old.values), new=formatVolume(new)))
