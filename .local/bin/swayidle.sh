bacb42c7bba13a18b7500c38f9bdcaf63102a06c
Sam Mason <sam@samason.uk>
2021-04-21 12:50:34 +0100

    sway updates

---
+++ .local/bin/swayidle.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -euf -o pipefail
+
+lock="swaylock -f -F -c 000000"
+
+exec swayidle -w \
+     timeout 300 "$lock" \
+     timeout 600 'swaymsg "output * dpms off"' \
+     resume 'swaymsg "output * dpms on"' \
+     before-sleep "$lock" \
+     lock "$lock"
