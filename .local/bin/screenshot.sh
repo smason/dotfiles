ec5ab00544ec1e7c54077f8eff2ffc4c60b073bc
Sam Mason <sam@samason.uk>
2022-06-10 18:13:39 +0100

    screenshot current window via grim

---
+++ .local/bin/screenshot.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+region="$(swaymsg -t get_tree | jq -j '.. | select(.type?) | select(.focused).rect | "\(.x),\(.y) \(.width)x\(.height)"')"
+
+echo "capturing $region"
+
+exec grim -g "$region"
