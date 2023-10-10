6872a20837b79f683cbe973880242f872c82befa
Sam Mason <sam@samason.uk>
2023-10-10 15:32:23 +0100

    improve fade times

--- .local/bin/swayidle.sh
+++ .local/bin/swayidle.sh
@@ -5,7 +5,7 @@ set -eufo pipefail
 lock=swaylock.sh
 
 exec swayidle -w \
-     timeout 300 "$lock fade" \
+     timeout 300 "$lock slow" \
      timeout 600 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
      before-sleep "$lock" \
-     lock "$lock"
+     lock "$lock fast"
--- .local/bin/swaylock.sh
+++ .local/bin/swaylock.sh
@@ -4,7 +4,8 @@ set -eufo pipefail
 
 case "${1:-}" in
   # minimal screen fadeout, https://git.sr.ht/~emersion/chayang
-  fade) chayang;;
+  slow) chayang -d 10;;
+  fast) chayang -d 0.3;;
 esac
 
 exec swaylock -f -F -c 000000
