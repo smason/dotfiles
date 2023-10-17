7e8aa8091b41a604af3d604ee291c54173e29465
Sam Mason <sam@samason.uk>
2023-10-17 18:35:06 +0100

    use AutoEq ParametricEQ text files for input

--- .local/bin/oratory1990-eq-to-pipewire-conf.py
+++ .local/bin/oratory1990-eq-to-pipewire-conf.py
@@ -1,62 +1,114 @@
 import argparse
-import csv
+import json
+import pathlib
 import re
+import string
+import urllib.parse
 from dataclasses import dataclass
+from itertools import pairwise
+
 
 @dataclass
 class Filter:
+    name: str
     filter: str
     freq: str
     gain: str
     qfactor: str
 
-def field_parser(regex):
-    compiled = re.compile(regex)
-    def inner(text):
-        match = compiled.match(text)
-        assert match
-        return match.group(1)
-    return inner
-
-
-P_FREQ = field_parser(r'^([0-9]+) Hz$')
-P_GAIN = field_parser(r'^(-?[0-9]+\.[0-9]+) dB$')
-P_QF = field_parser(r'^(-?[0-9]+\.[0-9]+)$')
-
 
 FTYPES = {
-    "PEAK": "bq_peaking",
-    "LOW_SHELF": "bq_lowshelf",
-    "HIGH_SHELF": "bq_highshelf",
+    "PK": "bq_peaking",
+    "LSC": "bq_lowshelf",
+    "HSC": "bq_highshelf",
 }
 
+# for parsing ParametricEQ.txt files from https://github.com/jaakkopasanen/AutoEq e.g. see
+#   https://github.com/jaakkopasanen/AutoEq/blob/master/results/oratory1990/over-ear/Sony%20MDR-7506/Sony%20MDR-7506%20ParametricEQ.txt
+RE_FILTER = (
+    r"Filter [0-9]+: ON (?P<filter>[A-Z]+) "
+    r"Fc (?P<freq>[0-9]+(?:\.[0-9]+)?) Hz "
+    r"Gain (?P<gain>-?[0-9]+(?:\.[0-9]+)?) dB "
+    r"Q (?P<qf>[0-9]+(?:\.[0-9]+)?)"
+)
 
-def parse_file(path: str):
-    rows = csv.reader(open(path))
-    header = next(rows)
-    yield Filter("bq_highshelf", "0.0", P_GAIN(header[-1]), "1.0")
-    for _, ftype, freq, gain, qf, _ in rows:
-        yield Filter(FTYPES[ftype], P_FREQ(freq), P_GAIN(gain), P_QF(qf))
+MODULE_DEF = string.Template("""\
+context.modules = [{ name = libpipewire-module-filter-chain args={
+    node.description = $description
+    audio.channels = 2
+    audio.position = [ FL FR ]
+    capture.props = { node.name="effect_input.$name" media.class="Audio/Sink" }
+    playback.props = { node.name="effect_output.$name" node.passive=true }
+    filter.graph = {
+        nodes = [\n$nodes\
+        ]
+        links = [\n$links\
+        ]
+    }
+}}]
+""")
+
+def parse_file(fd):
+    m = re.match(r"Preamp: (?P<gain>-?[0-9]+(?:\.[0-9]+)) dB", next(fd))
+    yield Filter("preamp", "bq_highshelf", "0.0", m.group("gain"), "1.0")
+    for i, line in enumerate(fd, 1):
+        name = f"eq_band_{i}"
+        m = re.match(RE_FILTER, line)
+        filter = FTYPES[m.group("filter")]
+        yield Filter(
+            name, filter, m.group("freq"), m.group("gain"), m.group("qf")
+        )
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Translate from oratory1990 EQ presets to Pipewire config",
+        description="Translate from AutoEq ParametricEq files to Pipewire config",
     )
-    parser.add_argument("csv", help="CSV table extracted from PDF")
-    return parser.parse_args()
+    parser.add_argument("source", help="Text file from AutoEq repo")
+    parser.add_argument("--description", "--desc", "-d", help="Human readable description")
+    parser.add_argument("--name", "-c", help="Machine compatible identifier")
+    args = parser.parse_args()
+    name = urllib.parse.unquote(pathlib.Path(args.source).name)
+    m = re.match(r"(.*) ParametricEQ.txt", name)
+    # try and fill these in if the user didn't supply them
+    if not args.description:
+        args.description = f"{m.group(1)} Equaliser"
+    if not args.name:
+        keep = re.findall(r"([a-z0-9]+)", m.group(1), re.IGNORECASE)
+        args.name = '_'.join(keep).lower()
+    return args
 
 
 def main():
     args = parse_args()
-    for band, f in enumerate(parse_file(args.csv), 1):
-        node = (
-            "{ "
-            f"type=builtin name=eq_band_{band} label={f.filter} "
-            f'control={{ "Freq" = {f.freq} "Gain" = {f.gain} "Q" = {f.qfactor} }}'
-            " }"
+    with open(args.source) as fd:
+        filters = list(parse_file(fd))
+
+    nodes = []
+    links = []
+
+    for a, b in pairwise(filters):
+        out = json.dumps(f"{a.name}:Out")
+        inp = json.dumps(f"{b.name}:In")
+        links.append(
+            f"{{ output={out} input={inp} }}\n"
         )
-        print(node)
+
+    for f in filters:
+        band = json.dumps(f.name)
+        nodes.append(
+            "{ "
+            f"type=builtin name={band} label={f.filter} "
+            f"control={{ Freq = {f.freq} Gain = {f.gain} Q = {f.qfactor} }}"
+            " }\n"
+        )
+
+    print(MODULE_DEF.substitute(
+        description=json.dumps(args.description),
+        name=args.name,
+        nodes="".join(nodes),
+        links="".join(links),
+    ))
 
 
 if __name__ == "__main__":
