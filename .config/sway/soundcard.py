a7507341fa9eae660ebded1eda315f3776b2e161
Sam Mason <sam@samason.uk>
2026-02-03 10:16:53 +0000

    sway fiddling

--- .config/sway/soundcard.py
+++ .config/sway/soundcard.py
@@ -1,44 +1,29 @@
 from argparse import ArgumentParser
 
-from alsaaudio import PCM_PLAYBACK, VOLUME_UNITS_RAW, Mixer, card_indexes, card_name
-
-CARDS = {card_name(i)[0]: i for i in card_indexes()}
+from pulsectl import Pulse
 
 
 def parseargs():
     parser = ArgumentParser()
     parser.add_argument("mode", choices={"mute", "micmute", "lower", "raise"})
-    parser.add_argument("--card", default="HDA Intel PCH", choices=CARDS.keys())
     return parser.parse_args()
 
 
 def main():
     args = parseargs()
 
-    def mixer(control):
-        return Mixer(control, cardindex=CARDS[args.card])
+    pulse = Pulse("sway-volume-tool")
+    sink = pulse.sink_default_get()
+    source = pulse.source_default_get()
 
     if args.mode == "mute":
-        m = mixer("Master")
-        m.setmute(not any(m.getmute()))
+        pulse.mute(sink, mute=not sink.mute)
     elif args.mode == "micmute":
-        m = mixer("Capture")
-        cur = any(v > 0 for v in m.getrec())
-        m.setrec(not cur)
+        pulse.mute(source, mute=not source.mute)
     elif args.mode == "lower":
-        m = mixer("Master")
-        lo, hi = m.getrange(PCM_PLAYBACK, units=VOLUME_UNITS_RAW)
-        assert lo == 0
-        [level] = m.getvolume(PCM_PLAYBACK, units=VOLUME_UNITS_RAW)
-        level -= max(level // 10, 1)
-        m.setvolume(max(level, 0), PCM_PLAYBACK, units=VOLUME_UNITS_RAW)
+        pulse.volume_change_all_chans(sink, -0.05)
     elif args.mode == "raise":
-        m = mixer("Master")
-        lo, hi = m.getrange(PCM_PLAYBACK, units=VOLUME_UNITS_RAW)
-        assert lo == 0
-        [level] = m.getvolume(PCM_PLAYBACK, units=VOLUME_UNITS_RAW)
-        level += max(level // 10, 1)
-        m.setvolume(min(level, hi), PCM_PLAYBACK, units=VOLUME_UNITS_RAW)
+        pulse.volume_change_all_chans(sink, +0.05)
     else:
         assert False, args.mode
 
